411b95beb31d35880c69edf9063c4ef4f54f28fe
Pouman1 <poutine503@gmail.com>
2024-01-25 16:58:07 -0500

    Pivot Visualizer completed

--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems.pivot;
 
+
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pivot extends SubsystemBase {
@@ -17,10 +20,28 @@ public class Pivot extends SubsystemBase {
          new ArmFeedforward(PivotConstants.kA, PivotConstants.kG, PivotConstants.kS, PivotConstants.KV);
     private double goal = 0;
 
+    PivotVisualizer visualizer;
+
+    private static Pivot instance;
+
+    public static Pivot getInstance(){
+        return instance;
+    }
+
+    public static Pivot initialize(PivotIO io){
+        if(instance == null){
+            instance = new Pivot(io);
+        }
+        return instance;
+    }
+
+
     public Pivot(PivotIO pivotIO) {
         io = pivotIO;
+        visualizer = new PivotVisualizer("Pivot", Color.kDarkOrange);
         io.updateInputs(inputs);
         goal = inputs.pivotRelativePosition;
+        visualizer.update(360 * inputs.pivotRelativePosition);
     }
 
     public void setGoal(double goal) {
@@ -31,6 +52,7 @@ public class Pivot extends SubsystemBase {
     public void periodic() {
         io.updateInputs(inputs);
         Logger.processInputs("Pivot", inputs);
+        visualizer.update(360 * inputs.pivotRelativePosition);
         io.rotatePivot(controller.calculate(goal, inputs.pivotRelativePosition) + 
             pivotFeedForward.calculate(controller.getSetpoint().position, controller.getSetpoint().position)); 
 
