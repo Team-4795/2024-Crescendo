719026c0e702c130498db9d0a1831d5886326dc0
Thomas Jin <83102001+Bob127227311@users.noreply.github.com>
2024-02-01 21:58:40 -0500

    added 3d visualization for shooter and pivot

--- src/main/java/frc/robot/subsystems/Shooter/ShooterVisualizer.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterVisualizer.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.Shooter;
 
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
@@ -7,21 +12,21 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public class ShooterVisualizer {
-    String name;
     Mechanism2d mechanism2d;
     MechanismRoot2d mechanismIsRoot;
     MechanismLigament2d shooter;
 
     public ShooterVisualizer(String name, Color color) {
-        this.name = name;
-
         mechanism2d = new Mechanism2d(0, 0);
         mechanismIsRoot = mechanism2d.getRoot("Shooter", 0, 0);
         shooter = mechanismIsRoot.append(new MechanismLigament2d("Shooter", 0, 0, 0, new Color8Bit(color)));
 
     }
 
-    public void update(double speed) {
+    public void update() {
+        Logger.recordOutput("Shooter2d", mechanism2d);
 
+        var shooterPose = new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0));
+        Logger.recordOutput("Shooter3D", shooterPose);
     }
 }
