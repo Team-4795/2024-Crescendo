a078e00a62255f5df36cca8519409f099cf775dc
Alan Liu <liualan998@gmail.com>
2024-02-03 13:23:35 -0500

    Auto fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -144,6 +144,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return null;
+    return autoChooser.get();
   }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems.intake;
 
 public final class IntakeConstants {
-    public static final int frontCanID = 2;
-    public static final int backCanID = 1;
+    public static final int frontCanID = 17;
+    public static final int backCanID = 18;
 
     public static final double simGearRatio = 30;
     public static final double overrideSpeed = 0.8;
