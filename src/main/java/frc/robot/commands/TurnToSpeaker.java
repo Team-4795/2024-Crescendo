bb199b8487974cac94c625cbef519890693c625d
Alan Liu <liualan998@gmail.com>
2024-02-03 10:43:36 -0500

    PID rotation tuning

--- src/main/java/frc/robot/commands/TurnToSpeaker.java
+++ src/main/java/frc/robot/commands/TurnToSpeaker.java
@@ -12,20 +12,24 @@ import frc.robot.Constants.OIConstants;
 
 public class TurnToSpeaker {
     private static Vision vision = Vision.getInstance();
-    private static PIDController rotationPID = new PIDController(0.1, 0, 0); // Change Values
+    private static PIDController rotationPID = new PIDController(0.02, 0, 0.00025); // Change Values
     
     public static Command turnTowardsSpeaker(Drive drive){
         return Commands.run(
         () -> {
             double deltaY = vision.getSpeakerPos().getY() - drive.getPose().getY();
             // double angle = Math.asin(deltaY / vision.getDistancetoSpeaker(drive.getPose())) * 180/Math.PI;
-            double angle = OIConstants.m_driverController.getRightY() * Math.PI;
-            Logger.recordOutput("Angle", angle);
+            double angle = OIConstants.m_driverController.getRightY() * 30;
+            double driveHeading = drive.getHeading();
+            double output = rotationPID.calculate(driveHeading, angle);
+            Logger.recordOutput("Vision/drive heading", driveHeading);
+            Logger.recordOutput("Vision/angle", angle);
+            Logger.recordOutput("Vision/output", output);
 
             drive.drive(
                 -MathUtil.applyDeadband(OIConstants.m_driverController.getLeftY(), OIConstants.kDriveDeadband),
                 -MathUtil.applyDeadband(OIConstants.m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                rotationPID.calculate(drive.getHeading(), angle), 
+                MathUtil.clamp(output, -1, 1), 
                 true, true);
         }, drive);
     }
