42384d3bf4f671b13f73078f40d8d45a2da690d3
Aaron <42575013+SuperWinner50@users.noreply.github.com>
2024-02-11 12:37:24 -0500

    Put GyroIONavX back

---
+++ src/main/java/frc/robot/subsystems/MAXSwerve/GyroIONavX.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.MAXSwerve;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+
+public class GyroIONavX implements GyroIO {
+    private final AHRS gyro = new AHRS(SPI.Port.kMXP);
+
+    public GyroIONavX() {
+        reset();
+    }
+
+    public void updateInputs(GyroIOInputs inputs) {
+       //inputs.rollPositionRad = gyro.getRoll();
+        //inputs.pitchPositionRad = gyro.getPitch();
+        inputs.yaw = gyro.getRotation2d();
+        inputs.yawVelocity = gyro.getRate();
+    }
+
+    public void reset() {
+        gyro.reset();
+    }
+
+    public void setOffset(double offset) {
+        gyro.setAngleAdjustment(offset);
+    }
+}
