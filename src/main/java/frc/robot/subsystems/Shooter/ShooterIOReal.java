4c5737fbf33c97773942b418eef50bdb7e7d2c0e
ColinH0 <119466406+ColinH0@users.noreply.github.com>
2024-02-13 17:46:38 -0500

    Plays sound on enable

--- src/main/java/frc/robot/subsystems/Shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOReal.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.Shooter;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
@@ -8,6 +12,7 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class ShooterIOReal implements ShooterIO {
     
@@ -15,8 +20,12 @@ public class ShooterIOReal implements ShooterIO {
     private TalonFX topShooterMotor = new TalonFX(ShooterConstants.rightCanID);
     private TalonFX bottomShooterMotor = new TalonFX(ShooterConstants.leftCanID);
 
+    private Orchestra m_orchestra = new Orchestra();
+
     private TalonFXConfiguration talonFXConfig = new TalonFXConfiguration();
     final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
+    private boolean isEnabled;
+    private boolean hasPlayed;
 
     public ShooterIOReal(){
         talonFXConfig.Slot0.kP = 0.05;
@@ -36,8 +45,6 @@ public class ShooterIOReal implements ShooterIO {
         // talonFXConfig.MotionMagic.MotionMagicCruiseVelocity = 10;
 
         talonFXConfig.Audio.BeepOnBoot = true;
-
-    
 
         bottomShooterMotor.clearStickyFaults();
         topShooterMotor.clearStickyFaults();
@@ -59,6 +66,13 @@ public class ShooterIOReal implements ShooterIO {
                             + " failed config with error "
                             + response.toString());
         }
+
+        hasPlayed = false;
+
+        m_orchestra.addInstrument(bottomShooterMotor);
+        m_orchestra.addInstrument(topShooterMotor);
+
+        m_orchestra.loadMusic("chirpWindowsXPFX.chrp");
     }
 
     @Override
@@ -77,5 +91,14 @@ public class ShooterIOReal implements ShooterIO {
         inputs.shooterMotorAppliedVolts = topShooterMotor.getMotorVoltage().getValueAsDouble();
         inputs.shooterMotorVelocityRPM = topShooterMotor.getVelocity().getValueAsDouble() * 60.0; // RPS to RPM
 
+        isEnabled = DriverStation.isEnabled();
+
+        if (isEnabled && !hasPlayed) {
+            m_orchestra.play();
+            hasPlayed = true;
+        }
+        else {
+            m_orchestra.stop();
+        }
     }
 }
