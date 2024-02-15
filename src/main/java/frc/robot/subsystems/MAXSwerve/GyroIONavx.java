a837ddfe5689fef39a92e30f01c1ce7fbb9f1db8
GooseJuice898 <oscarburgess262@gmail.com>
2024-02-15 16:30:01 -0500

    Update GyroIONavx.java

--- src/main/java/frc/robot/subsystems/MAXSwerve/GyroIONavx.java
+++ src/main/java/frc/robot/subsystems/MAXSwerve/GyroIONavx.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.MAXSwerve;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.SPI;
 
 public class GyroIONavX implements GyroIO {
@@ -14,7 +17,7 @@ public class GyroIONavX implements GyroIO {
     public void updateInputs(GyroIOInputs inputs) {
        //inputs.rollPositionRad = gyro.getRoll();
         //inputs.pitchPositionRad = gyro.getPitch();
-        inputs.yaw = gyro.getRotation2d();
+        inputs.yaw = Rotation2d.fromDegrees(-gyro.getAngle());
         inputs.yawVelocity = gyro.getRate();
     }
 
