2f20b71bc56d9d881af0d09feafd2572b7bf9f56
Aaron <millhouseaa@gmail.com>
2024-02-21 17:52:54 -0500

    Update talon fx status signal code & deps

--- src/main/java/frc/robot/subsystems/Shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOReal.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -22,8 +23,11 @@ public class ShooterIOReal implements ShooterIO {
     private boolean isEnabled;
     private boolean hasPlayed;
 
-    KrakenLogger topMotorLogger = new KrakenLogger(topShooterMotor, "Top shooter - ID " + ShooterConstants.rightCanID);
-    KrakenLogger bottomMotorLogger = new KrakenLogger(bottomShooterMotor, "Bottom shooter - ID " + ShooterConstants.leftCanID);
+    // KrakenLogger topMotorLogger = new KrakenLogger(topShooterMotor, "Top shooter - ID " + ShooterConstants.rightCanID);
+    // KrakenLogger bottomMotorLogger = new KrakenLogger(bottomShooterMotor, "Bottom shooter - ID " + ShooterConstants.leftCanID);
+
+    private final StatusSignal<Double> topRPM = topShooterMotor.getVelocity();
+    private final StatusSignal<Double> bottomRPM = bottomShooterMotor.getVelocity();
 
     public ShooterIOReal() {
         talonFXConfig.Slot0.kP = ShooterConstants.kP;
@@ -37,43 +41,41 @@ public class ShooterIOReal implements ShooterIO {
 
         talonFXConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
-        // talonFXConfig.MotionMagic.MotionMagicAcceleration = 100;
-        // talonFXConfig.MotionMagic.MotionMagicCruiseVelocity = 10;
-
         talonFXConfig.Audio.BeepOnBoot = true;
 
-        bottomShooterMotor.getMotorVoltage().setUpdateFrequency(50);
-        bottomShooterMotor.getVelocity().setUpdateFrequency(50);
-        bottomShooterMotor.getStatorCurrent().setUpdateFrequency(50);
-        bottomShooterMotor.getMotorVoltage().setUpdateFrequency(50);
-
-        topShooterMotor.getMotorVoltage().setUpdateFrequency(50);
-        topShooterMotor.getVelocity().setUpdateFrequency(50);
-        topShooterMotor.getStatorCurrent().setUpdateFrequency(50);
-        topShooterMotor.getMotorVoltage().setUpdateFrequency(50);
-
-        topShooterMotor.optimizeBusUtilization();
-        bottomShooterMotor.optimizeBusUtilization();
+        BaseStatusSignal.setUpdateFrequencyForAll(50,
+            topRPM,
+            bottomRPM);
 
         bottomShooterMotor.clearStickyFaults();
         topShooterMotor.clearStickyFaults();
 
-        StatusCode response = bottomShooterMotor.getConfigurator().apply(talonFXConfig);
-        if (!response.isOK()) {
+        StatusCode status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; i++) {
+            status = bottomShooterMotor.getConfigurator().apply(talonFXConfig);
+            if (status.isOK()) break;
+        }
+
+        if (!status.isOK()) {
             System.out.println(
                     "Talon ID "
                             + bottomShooterMotor.getDeviceID()
                             + " failed config with error "
-                            + response.toString());
+                            + status.toString());
         }
 
-        response = topShooterMotor.getConfigurator().apply(talonFXConfig);
-        if (!response.isOK()) {
+        status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; i++) {
+            status = topShooterMotor.getConfigurator().apply(talonFXConfig);
+            if (status.isOK()) break;
+        }
+
+        if (!status.isOK()) {
             System.out.println(
                     "Talon ID "
                             + topShooterMotor.getDeviceID()
                             + " failed config with error "
-                            + response.toString());
+                            + status.toString());
         }
 
         hasPlayed = false;
@@ -102,15 +104,17 @@ public class ShooterIOReal implements ShooterIO {
 
     @Override
     public void updateInputs(ShooterIOInputs inputs) {
-        inputs.bottomShooterMotorAppliedVolts = bottomShooterMotor.getMotorVoltage().getValueAsDouble();
-        inputs.bottomShooterMotorVelocityRPM = bottomShooterMotor.getVelocity().getValueAsDouble() * 60.0; // RPS to RPM
-        inputs.bottomShooterCurrent = bottomShooterMotor.getStatorCurrent().getValueAsDouble();
-        inputs.bottomShooterAppliedVolts = bottomShooterMotor.getMotorVoltage().getValueAsDouble();
+        BaseStatusSignal.refreshAll(topRPM, bottomRPM);
 
-        inputs.topShooterMotorAppliedVolts = topShooterMotor.getMotorVoltage().getValueAsDouble();
-        inputs.topShooterMotorVelocityRPM = topShooterMotor.getVelocity().getValueAsDouble() * 60.0; // RPS to RPM
-        inputs.topShooterCurrent = topShooterMotor.getStatorCurrent().getValueAsDouble();
-        inputs.topShooterAppliedVolts = topShooterMotor.getMotorVoltage().getValueAsDouble();
+        // inputs.bottomShooterMotorAppliedVolts = bottomShooterMotor.getMotorVoltage().getValueAsDouble();
+        inputs.bottomShooterMotorVelocityRPM = bottomRPM.getValueAsDouble() * 60.0; // RPS to RPM
+        // inputs.bottomShooterCurrent = bottomShooterMotor.getStatorCurrent().getValueAsDouble();
+        // inputs.bottomShooterAppliedVolts = bottomShooterMotor.getMotorVoltage().getValueAsDouble();
+
+        // inputs.topShooterMotorAppliedVolts = topShooterMotor.getMotorVoltage().getValueAsDouble();
+        inputs.topShooterMotorVelocityRPM = topRPM.getValueAsDouble() * 60.0; // RPS to RPM
+        // inputs.topShooterCurrent = topShooterMotor.getStatorCurrent().getValueAsDouble();
+        // inputs.topShooterAppliedVolts = topShooterMotor.getMotorVoltage().getValueAsDouble();
 
         isEnabled = DriverStation.isEnabled();
 
