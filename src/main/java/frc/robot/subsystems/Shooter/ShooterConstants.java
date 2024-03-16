5e999f7a478935f4bf72d7bcc3b7f99bde2f4000
Alan <150393046+Pouman1@users.noreply.github.com>
2024-03-16 12:08:33 -0400

    Update at 'Sat Mar 16 12:08:33 EDT 2024'

--- src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -3,12 +3,12 @@ package frc.robot.subsystems.Shooter;
 public final class ShooterConstants {
     public static final int leftCanID = 15;
     public static final int rightCanID = 16;
-
+    
     public static final double kP = 0.2;
     
     public static final double kVTop = 0.118;
-    public static final double kVBottom = 0.127;
-
+    public static final double kVBottom = 0.124;
+    
     public static final double RPMTolerance = 150;
     public static final double initialVelocity = 14; // m/s
 }
