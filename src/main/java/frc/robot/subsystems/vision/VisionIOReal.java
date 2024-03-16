783c116a06d08add66356f85ce35e69e39062e3d
Alan <150393046+Pouman1@users.noreply.github.com>
2024-03-16 09:05:11 -0400

    Update at 'Sat Mar 16 09:05:11 EDT 2024'

--- src/main/java/frc/robot/subsystems/vision/VisionIOReal.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOReal.java
@@ -61,11 +61,11 @@ public class VisionIOReal implements VisionIO {
             new Rotation3d(
                 0, 
                 Units.degreesToRadians(20), 
-                Units.degreesToRadians(-110)));
+                Units.degreesToRadians(110)));
 
         barbaryFigRobotToCam = new Transform3d(
             new Translation3d(
-                Units.inchesToMeters(-16),
+                Units.inchesToMeters(-11.5),
                 Units.inchesToMeters(5), 
                 Units.inchesToMeters(11)), 
             new Rotation3d(
@@ -81,7 +81,7 @@ public class VisionIOReal implements VisionIO {
             new Rotation3d(
                 0, 
                 Units.degreesToRadians(20), 
-                Units.degreesToRadians(110)));  
+                Units.degreesToRadians(-110)));  
 
         try {
             aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
