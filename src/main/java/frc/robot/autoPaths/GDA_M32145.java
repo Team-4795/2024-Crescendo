c2750cdfd5554ff7e046dc79aaa5321228f6a967
Thomas Jin <83102001+Bob127227311@users.noreply.github.com>
2024-03-23 15:20:30 -0400

    Sequenced a 6 note from mid

---
+++ src/main/java/frc/robot/autoPaths/GDA_M32145.java
@@ -0,0 +1,66 @@
+package frc.robot.autoPaths;
+
+import java.util.List;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.AutoCommands;
+
+public class GDA_M32145 {
+    private static List<PathPlannerPath> paths;
+    PathPlannerAuto auto;
+
+    public static Command load(){
+        paths = PathPlannerAuto.getPathGroupFromAutoFile("GDA M GP 32145");
+
+        return Commands.sequence(
+            AutoCommands.aimSpeakerDynamic(true),
+            AutoCommands.score(),
+
+        Commands.sequence(
+            AutoCommands.intakeTrajectory(paths.get(0)),
+            Commands.waitSeconds(0.2), 
+            AutoCommands.aimSpeakerDynamic(true)),
+
+        AutoCommands.score(),
+            
+        Commands.sequence(
+            AutoCommands.intakeTrajectory(paths.get(1)),
+            Commands.waitSeconds(0.2), 
+            AutoCommands.aimSpeakerDynamic(true)),
+
+            AutoCommands.score(),
+            
+        Commands.sequence(
+            AutoCommands.intakeTrajectory(paths.get(2)),
+            Commands.waitSeconds(0.2)),
+            AutoCommands.aimSpeakerDynamic(true),
+
+            AutoCommands.score(),
+            
+            Commands.sequence(
+                AutoCommands.intakeTrajectory(paths.get(3)),
+                Commands.deadline(
+                    Commands.sequence(
+                        AutoCommands.followTrajectory(paths.get(4)), 
+                        Commands.waitSeconds(0.2)
+                    ), AutoCommands.aimSpeakerDynamic(true)
+                ),
+                AutoCommands.score()
+            ).until(() -> AutoGamepieces.isGone(4)),
+
+            Commands.sequence(
+                AutoCommands.intakeTrajectory(paths.get(5)),
+                Commands.deadline(
+                    Commands.sequence(
+                        AutoCommands.followTrajectory(paths.get(6)), 
+                        Commands.waitSeconds(0.2)
+                    ), AutoCommands.aimSpeakerDynamic(true)
+                ),
+                AutoCommands.score()
+            ).until(() -> AutoGamepieces.isGone(5)));
+
+    }
+}
